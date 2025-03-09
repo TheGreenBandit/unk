4c6cfb4583e398ad1811ef475af0594ed8f5cd48
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2025-03-09 19:46:12 -0400

    added version

--- Unk/Unk.cs
+++ Unk/Unk.cs
@@ -15,6 +15,7 @@ namespace Unk
 {
     public class Unk : MonoBehaviour
     {
+        public const string VERSION = "1.0";
         private List<ToggleCheat> cheats;
         private Harmony harmony;
         private UnkMenu menu;
